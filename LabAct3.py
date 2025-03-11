f3ef8e6beabfa184e3ed112b06ff5951d9b07692
dabidii <david23rigos@gmail.com>
2025-03-11 22:54:42 +0800

    Add files via upload

---
+++ LabAct3.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you?"))
+if age >= 100:
+	print("Ho is you ancient?! or ...")
+elif age >= 65:
+	print("You're a Senior, Long Live!.")
+elif age >= 20:
+	print("You are an adult, good luck!.")
+elif age >= 13:
+	print("You are a teenager, enjoy life.")
+elif age >= 0:
+	print("You are a child, hope your childhood will be good.")
+else:
+	print("Invalid Age")
